37de5de9673fa66c546c6591f41906331373e7e4
Lazaro <alfonso.lazaro@onmobile.com>
2020-03-03 17:56:24 +0100

    Add project files.

---
+++ Lz.Microsoft.Configuration.LegacyProvider/Extension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace Lz.Microsoft.Configuration.LegacyProvider
+{
+	public static class Extension
+	{
+		//
+		// Summary:
+		//     Adds the XML web.config/app.config configuration provider to builder. Specifically "AppSettings" and "ConnectionStrings" sections only.
+		//
+		// Parameters:
+		//   builder:
+		//     The Microsoft.Extensions.Configuration.IConfigurationBuilder to add to.
+		//
+		// Returns:
+		//     The Microsoft.Extensions.Configuration.IConfigurationBuilder.
+		public static IConfigurationBuilder AddLegacyConfig(this IConfigurationBuilder builder, IEnumerable<string> sectionDelimiters = null)
+		{
+			return builder.Add(new LegacyConfigurationProvider(sectionDelimiters));
+		}
+
+		/*
+		public static IConfigurationBuilder AddAppConfig(this IConfigurationBuilder builder, IEnumerable<string> sectionDelimiters = null)
+		{
+			return builder.Add(new LegacyConfigurationProvider(sectionDelimiters));
+		}
+
+		public static IConfigurationBuilder AddWebConfig(this IConfigurationBuilder builder, IEnumerable<string> sectionDelimiters = null)
+		{
+			return builder.Add(new LegacyConfigurationProvider(sectionDelimiters));
+		}
+		*/
+
+	}
+}
---
+++ Lz.Microsoft.Configuration.LegacyProvider/LegacyConfigurationProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+
+namespace Lz.Microsoft.Configuration.LegacyProvider
+{
+    /// <summary>
+    /// Configuration provider for IConfigurationBuilder that adds the config from the old web.config/app.config legacy configurations.
+    /// </summary>
+    public class LegacyConfigurationProvider : ConfigurationProvider, IConfigurationSource
+    {
+        private IEnumerable<string> _sectionDelimiters = new String[] { ".", ",", "-"};
+
+        /// <summary>
+        /// Configuration provider for IConfigurationBuilder that adds the config from the old web.config/app.config legacy configurations.
+        /// </summary>
+        /// <param name="sectionDelimiters">Specify what characters will be treated as section delimiters, by default it uses : . , - </param>
+        public LegacyConfigurationProvider(IEnumerable<string> sectionDelimiters = null)
+        {
+            if (sectionDelimiters != null)
+            {
+                this._sectionDelimiters = sectionDelimiters;
+            }
+        }
+
+        public override void Load()
+        {
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                Data.Add($"ConnectionStrings:{connectionString.Name}", connectionString.ConnectionString);
+            }
+
+            foreach (var settingKey in ConfigurationManager.AppSettings.AllKeys)
+            {
+                var settingKeyWithDelimiters = Regex.Replace(settingKey, "[" + Regex.Escape(String.Join("", _sectionDelimiters)) + "]", ":");
+                Data.Add(settingKeyWithDelimiters, ConfigurationManager.AppSettings[settingKey]);
+            }
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return this;
+        }
+    }
+}
